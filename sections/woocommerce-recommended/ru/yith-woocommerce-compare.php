ad7b673a1952aceea470ba2478ac63f988b87684
maksim <maksim.info.555@gmail.com>
2015-10-27 09:27:16 +0200

    UPD: optimization

---
+++ sections/woocommerce-recommended/ru/yith-woocommerce-compare.php
@@ -0,0 +1,29 @@
+<h3>Yith woocommerce compare</h3>
+
+<p>
+    YITH WooCommerce Compare allows you to compare more products of your shop in one complete table.
+</p>
+
+<p>
+    YITH WooCommerce Compare plugin is an extension of WooCommerce plugin that allow your users to compare some products of your shop. All products are saved in one complete table where the user can see the difference between the products. Each product features can be set with the woocommerce attributes in the product configuration. You can also add a simple widget with the list of products the users have added, where you can manage them. Also you can simply customize the compare table with your style, by editing the specific template.
+</p>
+
+<h4>Configuration</h4>
+
+<ul class="marked-list">
+    <li>
+        Add the features in each product by setting the attributes in prouct configuration;
+    </li>
+    <li>
+        Configure the options in YIT Plugin -> Compare;
+    </li>
+    <li>
+        The user of your shop will click in the "Compare" button located in the single product page;
+    </li>
+    <li>
+        Will be open a popup with the compare table inside.
+    </li>
+</ul>
+<p>
+    You can also add the widget with the product list of compare table, in Appearance -> Widgets and adding the widget "YITH WooCommerce Compare Widget".
+</p>
