08f41cd77b49bd3834bc8f946966938a6b3da8e2
maksim <maksim.info.555@gmail.com>
2015-11-05 17:25:48 +0200

    TEST: img

--- sections/woocommerce-recommended/en/yith-woocommerce-wishlist.php
+++ sections/woocommerce-recommended/en/yith-woocommerce-wishlist.php
@@ -8,3 +8,5 @@
 </p>
 
 <figure class="img-polaroid"><img src="img/woocommerce/woocommerce-yith-wishlist-panel.png" alt=""></figure>
+
+<figure class="img-polaroid"><img src="img/woocommerce/woocommerce-yith-wishlist-panel.png" alt=""></figure>
