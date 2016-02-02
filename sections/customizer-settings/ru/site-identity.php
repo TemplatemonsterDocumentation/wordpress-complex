18a18cd031de7fad5304b0e3200f3669642f89eb
maksim <maksim.info.555@gmail.com>
2016-02-02 17:57:20 +0200

    NEW: site-identity, static-front-page, typography

---
+++ sections/customizer-settings/ru/site-identity.php
@@ -0,0 +1,15 @@
+<h3>Site Identity</h3>
+<p>Здесь вы можете настроить айдентику сайта</p>
+
+<figure class="img-polaroid">
+    <img src="img/tm/customizer/.png" alt="" >
+</figure>
+
+<h5>Site title</h5>
+
+<p>Title для вашего сайта.</p>
+
+
+<h5>Tagline</h5>
+
+<p>This a short statement similar to a subtitle or advertising slogan</p>
