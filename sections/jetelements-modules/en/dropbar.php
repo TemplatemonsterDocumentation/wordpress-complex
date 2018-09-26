e3f191635e7551cd50e73d439f27bf0cf1e8a808
blueberry <aliablueberry.18@gmail.com>
2018-09-26 17:05:08 +0300

    dropbar jetelements 1.13

---
+++ sections/jetelements-modules/en/dropbar.php
@@ -0,0 +1,129 @@
+<h2>Dropbar</h2>
+
+<h6>
+Dropbar widget helps you to display additional information in compact form using Elementor templates or simple text.
+
+<h4>Content</h4>
+<h5>Dropbar</h5>
+
+
+
+<h6>Button</h6>
+
+<ul class="marked-list">
+<p><li><b>Text</b> — in this field you can insert text, which will be displayed on the button.</li></p>
+<p><li><b>Before Icon</b> — here you can select the icon from dropdown list, which will be showcased before the button.</li></p>
+<p><li><b>After Icon</b> — here you can select the icon from dropdown list, which will be placed after the button.</li></p>
+<p><li><b>Alignment</b> — define the appropriate button alignment. The button can be located to the left, on the centre, to the right or justified.</li></p></ul>
+
+<h6>Content</h6>
+
+<ul class="marked-list">
+<p><li><b>Content Type</b> — here you can select one of the available options: <b>Simple Text</b> or <b>Template</b>. Simple Text allows filling the dropdown section with suitable text. Template allows choosing one of the templates in dropdown list, which will be displayed in dropdown section.</li></p></ul>
+
+<h5>Settings</h5>
+
+<ul class="marked-list">
+<p><li><b>Position</b> — here you can adjust the dropbar's posiiton on the page on desktop, laptop and mobile phone.</li></p>
+<p><li><b>Mode</b> — here you can define if the dropbar content opens on mouse hover or on mouse click.</li></p>
+<p><li><b>Hide Delay</b> — it is available to define the time when the dropdown section will be hidden.</li></p>
+<p><li><b>Show Effect</b> — here you can add one of the effects how the drowdown section will appear.</li></p>
+<p><li><b>Offset</b> — here you can tune the dropdown content's offset down.</li></p>
+<p><li><b>Width</b> — define the dropdown section's width .</li></p>
+<p><li><b>Fixed Layout</b> — enable this option if you want to fix position of the dropbar on the page.</li></p></ul>
+
+<ul class="marked-list">
+<p><li><b>Fixed Position</b> — here you can determine the dropbar's position on the page.</li></p>
+<p><li><b>Gap</b> — here you can fix the gap larger or smaller around the dropbar</li></p>
+<p><li><b>Z-Index</b> — you can specify the dropbar's stack order relatively to other elements on the page.</li></p></ul>
+
+
+<h4>Style</h4>
+
+<h5>Button</h5>
+
+<ul class="marked-list">
+<p><li><b>Typography</b> — turn the option on to view the typography settings. They are similar to the typography settings of other JetElements modules.</li></p>
+<p><li><b>Icon Font Size</b> — here you can adjust the icon's size.</li></p>
+<p><li><b>Icon Spacing</b> — here it is available to define the space between the icon and the text on the button.
+
+
+<p>Here you can switch from customizing <b>Normal</b> to <b>Hover</b> Dropbar style settings. Just click on the button Hover to proceed to customizing hover elements.</p>
+
+<ul class="marked-list">
+
+<p><li><b>Background Type</b> — in this block you can choose, whether you want to use classic or gradient type for the dropbar. The customization options vary for each type.
+
+
+<h6>Classic Background Type</h6>
+
+<ul class="marked-list">
+<p><li><b>Color</b> — here you can pick the dropbar's colour using color picker tool.</li></p>
+<p><li><b>Image</b> — here you can choose the image you want to use as a background. Select the image from the media library or upload it to define it as a background in this block. When the image is uploaded, you’ll be able to define its position, alignment, turn on repeat and set the necessary size.</li></p>
+<p><li><b>Text Color</b> — here you can define the suitable color for dropbar's text using color picker tool.</li></p>
+<p><li><b>Icon Color</b> — here you can define the icon's color using color picker tool.</li></p>
+<p><li><b>Box Shadow</b> — enable this option if you want to access the shadow advanced settings for this module, and need to apply shadow for it.</li></p></ul>
+
+<h6>Gradient Background Type</h6>
+
+<ul class="marked-list">
+<p><li><b>Color</b> —  here you can set background color for the dropbar using color picker tool.</li></p>
+<p><li><b>Location</b> — here you can define the proportion for the first color in the gradient background.</li></p>
+<p><li><b>Second Color</b> — here you can set the second color to use in the gradient background.</li></p>
+<p><li><b>Location</b> — here you can define the proportion for the second color in the gradient background.</li></p>
+<p><li><b>Type</b> — define, if you want to use linear or radial type of the gradient.</li></p>
+<p><li><b>Angle</b> — in this block you can set the angle for the gradient (in degrees).</li></p>
+<p><li><b>Text Color</b> — here you can define the suitable color for dropbar's text using color picker tool.</li></p>
+<p><li><b>Box Shadow</b> — enable this option if you want to access the shadow advanced settings for this module, and need to apply shadow for it.</li></p></ul></li></p></ul>
+
+<ul class="marked-list">
+
+<p><li><b>Hover Animation</b> — this option is available in Hover Dropbar style settings block. here you can add animation effect to the dropbar on mouse hover.</p></li></p></ul></li></p></ul>
+
+<ul class="marked-list">
+
+<p><li><b>Border Type</b> —  here you can define the type of the border you need to use for the element.</li></p> 
+<p><li><b>Border Radius</b> — here you can set the border radius for the dropbar.</li></p>
+<p><li><b>Padding</b> — here you can set the preferable custom padding for the module. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</li></p></ul>
+
+<h5>Content</h5>
+
+<ul class="marked-list">
+<p><li><b>Typography</b> — turn the option on to view the typography settings.</li></p>
+<p><li><b>Alignment</b> — set the appropriate alignment of dropbar's content.</li></p>
+<p><li><b>Background Type</b> — in this block you can choose, whether you want to use classic or gradient type for the dropbar's content. The customization options vary for each type.</li></p>
+
+<ul class="marked-list">
+
+<h6>Classic Background Type</h6>
+
+<ul class="marked-list">
+<p><li><b>Color</b> — here you can pick the colour for the dropbar's content using color picker tool.</li></p>
+<p><li><b>Image</b> — here you can choose the image you want to use as a background in dropdown section. Select the image from the media library or upload it to define it as a background in this block. When the image is uploaded, you’ll be able to define its position, alignment, turn on repeat and set the necessary size.</li></p>
+<p><li><b>Text Color</b> — here you can define the suitable color for text in the dropdown section using color picker tool.</li></p>
+<p><li><b>Border Type</b> — here you can define the needed border type for the dropdown section. It can be dotted, dashed, double or solid. </li></p>
+<p><li><b>Width</b> — define the width of the borders in the dropdown section.</li></p>
+<p><li><b>Color</b> — define the color of the borders in the dropdown section.</li></p>
+<p><li><b>Border Radius</b> — here you can set the radius of the borders in the dropdown section.</li></p>
+<p><li><b>Box Shadow</b> — enable this option if you want to access the shadow advanced settings for this module, and need to apply shadow for it.</li></p>
+<p><li><b>Padding</b> — here you can set the preferable custom padding for the module. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</li></p>
+<p><li><b>Z-Index</b> — you can specify the dropbar's content's stack order relatively to other elements on the page.</li></p></ul>
+
+
+<h6>Gradient Background Type</h6>
+
+<ul class="marked-list">
+<p><li><b>Color</b> —  here you can set background color for the dropbar using color picker tool.</li></p>
+<p><li><b>Location</b> — here you can define the proportion for the first color in the gradient background.</li></p>
+<p><li><b>Second Color</b> — here you can set the second color to use in the gradient background.</li></p>
+<p><li><b>Location</b> — here you can define the proportion for the second color in the gradient background.</li></p>
+<p><li><b>Type</b> — define, if you want to use linear or radial type of the gradient.</li></p>
+<p><li><b>Angle</b> — in this block you can set the angle for the gradient (in degrees).</li></p>
+<p><li><b>Text Color</b> — here you can define the suitable color for text in the dropdown section using color picker tool.</li></p>
+<p><li><b>Border Type</b> — here you can define the needed border type for the dropdown section. It can be dotted, dashed, double or solid. </li></p>
+<p><li><b>Width</b> — define the width of the borders in the dropdown section.</li></p>
+<p><li><b>Color</b> — define the color of the borders in the dropdown section.</li></p>
+<p><li><b>Border Radius</b> — here you can set the radius of the borders in the dropdown section.</li></p>
+<p><li><b>Box Shadow</b> — enable this option if you want to access the shadow advanced settings for this module, and need to apply shadow for it.</li></p>
+<p><li><b>Padding</b> — here you can set the preferable custom padding for the module. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</li></p>
+<p><li><b>Z-Index</b> — you can specify the dropbar's content's stack order relatively to other elements on the page.</li></p></ul>
