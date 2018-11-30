61f71254b55198580063f7a0a8fd9d2130fd7483
blueberry <aliablueberry.18@gmail.com>
2018-11-30 11:40:25 +0200

    jetsmartfilters changelog

---
+++ sections/jetsmartfilters-changelog/en/__description.php
@@ -0,0 +1,18 @@
+<h3>Changelog</h3>
+
+<h5>In this block you can view the latest updates for JetSmartFilters plugin.
+ </h5>
+
+
+<h4>Version 1.1.0 (November 30, 2018)</h4>
+
+<ul class="marked-list">
+<li>Added RU localization;</li>
+<li>Added an ability to edit or disable prev/next controls in Pagination widget;</li>
+<li>Added step, number format and suffix options for range and check range filters;</li>
+<li>Added an ability to search by meta field in search filter;</li>
+<li>Added an ability to run Elementor ready tiggers after appling filters;</li>
+<li>Added an ability to filter the same query variable by multiple filters.</li>
+</ul>
+
+
