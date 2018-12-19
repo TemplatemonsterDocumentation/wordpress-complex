75486f2d82da8293afa54b76017da98b841aaeec
blueberry <aliablueberry.18@gmail.com>
2018-12-19 17:55:29 +0200

    corrections in JetBlog and JetBlocks docs

--- sections/jetblog-settings/en/__description.php
+++ sections/jetblog-settings/en/__description.php
@@ -2,10 +2,10 @@
 
 <h5>In this block you can learn more about JetBlog settings. </h5>
 
-<p>To access JetElements settings you need to navigate to <b>Elementor > JetBlog Settings</b> in your Dashboard.</p>
+<p>To access JetBlog settings you need to navigate to <b>Elementor > JetBlog Settings</b> in your Dashboard.</p>
 
 <ul class="marked-list">
-<p><li><b>Youtube API Key</b> - here you can input an API key to use it for the Advanced Map widget. In the case you don't have it, please, create <a href="https://console.developers.google.com/apis/dashboard" rel="nofollow">here</a>.</li></p>
+<p><li><b>Youtube API Key</b> - here you can input an API key to use it for the Video Playlist widget. In the case you don't have it, please, create <a href="https://console.developers.google.com/apis/dashboard" rel="nofollow">here</a>.</li></p>
 <p><li><b>Smart Posts List: allow filters for post types</b> - this option allows selecting post types supported by Filter by Terms feature in Smart Posts List widget.</li></p>
 </ul>
 
