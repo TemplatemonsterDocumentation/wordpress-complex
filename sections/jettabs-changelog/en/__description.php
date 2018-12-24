2da8d20ce0ad3e719db0bea3896bfdb0823d7891
blueberry <aliablueberry.18@gmail.com>
2018-12-24 14:13:43 +0200

    updated version of JetTabs doc

--- sections/jettabs-changelog/en/__description.php
+++ sections/jettabs-changelog/en/__description.php
@@ -1,20 +1,28 @@
 <h3>Changelog</h3>
 
- <h4>Version 1.1.3 (September 6, 2018)</h4>
-
+<h4>Version 1.1.5 (December 24, 2018)</h4>
 <ul class="marked-list">
-<li>Added: multiple performance improvements and bug fixes.</li>
+<li> Added an ability to link tabs.</li>
+</ul>
+
+<h4>Version 1.1.4 (November 23, 2018)</h4>
+<ul class="marked-list">
+<li>Added RU localization;</li>
+<li>Added ability using dynamic tags in widgets;</li>
+<li>Added responsive tab controls position in the Tabs widget;</li>
+<li>Added multiple performance improvements and bug fixes.</li>
+
+ <h4>Version 1.1.3 (September 6, 2018)</h4>
+<ul class="marked-list">
+<li>Added multiple performance improvements and bug fixes.</li>
 </ul>
 
  <h4>Version 1.1.1 (July 4, 2018)</h4>
-
 <ul class="marked-list">
 <li>Added: Elementor 2.1 compatibility.</li>
 </ul>
 
-
  <h4>Version 1.1.0 (June 26, 2018)</h4>
-
 <ul class="marked-list">
 <li>Added new Switcher widget;</li>
 <li>Added JetTabs widget / auto tab switching option.</li>
