bfd641b333124b9eef8989ee6f3914d561856226
blueberry <aliablueberry.18@gmail.com>
2018-12-26 10:50:56 +0200

    jetreviews doc

--- sections/jetreviews-widget/en/review-widget-content.php
+++ sections/jetreviews-widget/en/review-widget-content.php
@@ -2,58 +2,61 @@
 
 <h6>In this block you can edit the content displayed with JetReview plugin.</h6>
 
-<h4>Content</h4>
+<h5>Content</h5>
 
  	<figure class="img-polaroid">
         <img src="projects/<?php echo $project;?>/img/inst/widg1.png">
   	</figure>
 
-<ol class="index-list">
-<p><li><b>Content Source</b> - here you can select, if you want to add content to the reviews widget manually, or use the post meta information (if it was previously added) to become the source of the widget’s content. </li></p>
-
-<p><li>In the case you’ve selected <b>Content Source > Post Meta</b>, you’ll need to define the <b>Post ID</b> in order for the widget to get the information from it. Please, feel free to read <a href="https://zemez.io/wordpress/support/how-to/find-page-post-id/" target="_blank">How to Find Out Page / Post ID</a> tutorial to learn how to find out the post ID.</li></p>
+<ul class="marked-list">
+<li><p><b>Content Source</b> - here you can select, if you want to add content to the reviews widget manually, or use the post meta information (if it was previously added) to become the source of the widget’s content. </p></li>
 
 <p><li>In the case you’ve selected <b>Manually Input</b> content source, you’ll need to fill in the following information.
 
 <ul class="marked-list">
-<p><li><b>Title</b> - here you can fill in the preferable <b>Review</b> widget’s title.</li></p>
-
+<p><li><b>Show Review Author</b> - you can enable this option if you want to display information about the author of the review.</li></p>
 <p><li><b>Add item</b> - click <b>Add Item</b> button to add the features which will be displayed in <b>Review</b> widget as the bars or rating categories.</li></p></ul></li></p>
-
 <p><li>Each of the items has the following additional fields you’ll need to fill in.
-
 <ul class="marked-list">
 <p><li><b>Label</b> - here you need to name the rating category (e.g., <b>Design, Quality,</b> etc.).</li></p>
-
 <p><li><b>Field Value</b> - here you need to input the value you want to assign to this rating category (e.g., 4).</li></p>
-
 <p><li><b>Field Max</b> - here you need to specify the maximum value for the rating category (e.g., 5, in this case the item will display 4 from 5 possible rating points).</li></p>
-
-
 <p><li><b>Summary Title</b> - here you need to fill in the title to use it for the review summary.</li></p>
 <p><li><b>Summary Description</b> - here you have to fill in the summary text in the content field (you can also use <b>html</b> markup in <b>Text</b> editor, add media, etc.).</li></p>
-<p><li><b>Summary Legend</b> - here you need to add the text to show up as the summary for the review.</li></p></ul></li></p></ol>
+<p><li><b>Summary Legend</b> - here you need to add the text to show up as the summary for the review.</li></p></ul>
+
+<p><li>In the case you’ve selected <b>Post Meta</b> content source, you’ll need to fill in the following information.
+<ul class="marked-list">
+<p><li><b>Get Data From Post</b> - here you need to define the <b>Post ID</b> in order for the widget to get the information from it. Please, feel free to read <a href="https://zemez.io/wordpress/support/how-to/find-page-post-id/" target="_blank">How to Find Out Page / Post ID</a> tutorial to learn how to find out the post ID.</li></p>
+<li><p>><b>Show Review Author</b> - you can enable this option if you want to display information about the author of the review.</p></li></ul></li>
+
 
 <h5>Structured Data</h5>
 
 <ul class="marked-list">
-	<p><li><b>Add Structured Data to Review Box</b> - enable this option in order to add the data markup for the review content and enable ranking and displaying it with Google.</li></p></ul>
+<p><li><b>Add Structured Data to Review Box</b> - enable this option in order to add the data markup for the review content and enable ranking and displaying it with Google.</li></p></ul>
 
+<h5>Header Settings</h5>
+<p>The header appears when there are at least 2 reviews.</p>
+<ul class="marked-list">
+<p><li><b>Average Layout</b> - here you can choose the preferable layout for the average rating (you can set <b>Points, Percentage</b> or <b>Stars</b>).</li></p>
+<p><li><b>Progressbar</b> - enable this option in order to display the average rating in the form of a progressbar.</li></p>
+<div class="alert alert-info">Note, that the progressbar is available only for <b>Percentage</b> and <b>Points</b> layout types.</div>
+</li></p>
+<p><li><b>Values Position</b> - here you can select the position of the average rating either inside or above the progress bar.</li></p></ul>
+	
 <h5>Fields Settings</h5>
 
 <ul class="marked-list">
 <p><li><b>Layout</b> - here you can select the preferable type of the rating to use it for the items with rating categories (you can set <b>Points, Percentage</b> or <b>Stars</b>).</li></p>
-
 <p><li><b>Progressbar</b> - enable this option in order to display the rating in the form of a progressbar.
 
 <div class="alert alert-info">Note, that the progressbar is available only for <b>Percentage</b> and <b>Points</b> layout types.</div>
 </li></p>
 
-<p><li><b>Values Position</b> - the rating value can be put above the progress bar or inside it.</li></p>
+<p><li><b>Values Position</b> - enable this option in order to display the rating in the form of a progressbar.</li></p>
 
-<p><li><b>Values Alignment</b> - in the case you’ve set <b>Values Position > Above Progressbar</b>, you can define if the value will be displayed on the left or on the right.</li></p>
-
-<p><li><b>Label Suffix</b> - in the case you want to add additional text to the existing item label, input it in this field.</li></p></ul>
+<p><li><b>Values Alignment</b> - in the case you’ve set <b>Values Position > Above Progressbar</b>, you can define if the value will be displayed on the left or on the right.</li></p></ul>
 
 <h5>Summary Settings</h5>
 
--- sections/jetreviews-widget/en/review-widget-style.php
+++ sections/jetreviews-widget/en/review-widget-style.php
@@ -6,11 +6,38 @@
         <img src="projects/<?php echo $project;?>/img/inst/widg2.png">
   	</figure>
 
-<h5>Title</h5>
+
+<h5>Review</h5>
 
 <ul class="marked-list">
-<li><p><b>Color</b> - here you can change the color of the title color.</p></li>
-<li><p><b>Typography</b> - turn the option on to view the typography settings.
+<li><p><b>Review Gap</b> - here you can fix the appropriate gap between reviews for different screen resolutions.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the review. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the review.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Box Shadow</b> - enable this option if you want to access the shadow advanced settings for review, and need to apply shadow for it.</p></li></ul>
+
+<h5>Header</h5>
+
+<ul class="marked-list">
+<li><p><b>Background Color</b> - here you can select the color to use it for the header background.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the header. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li>
+<li><p><b>Margin</b> - here you can apply your custom margins in pixels to set them for the header block.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the header.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li></ul>
+
+<h6>Title</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can pick color of the title.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li>
+<li><p><b>Alignment</b> - here you can define the preferable title alignment. The title can be placed to the left, to the right, or remain centered.</p></li>
+
+
+<h5>Total Average</h5>
+
+<h6>Value</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can change the color of the value in the average rating.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p>
 
 <ul class="marked-list">
 <li><p><b>Size</b> — define the font size of the title.</p></li>
@@ -19,31 +46,137 @@
 <li><p><b>Transform</b> — here you can choose from the dropdown menu, if you want the title to be shown in uppercase, lowercase, capitalize or normal way.</p></li>
 <li><p><b>Style</b> — in this block you can choose from the dropdown menu the style of the font. It can be normal, italic (the characters look similar to handwriting) and oblique (the characters are slightly inclined to the right).</p></li>
 <li><p><b>Line Height</b> — in this field you can set the height of the slide title line.</p></li>
-<li><p><b>Letter Spacing</b> — here you can set the space between letters.</p></li></ul>
+<li><p><b>Letter Spacing</b> — here you can set the space between letters.</p></li></ul></li>
 
-</p></li>
+<h6>Stars Rating</h6>
+<ul class="marked-list">
+<li><p><b>Stars Size</b> - here you can set different size for the stars for different screen resolutions.</p></li> 
+<li><p><b>Empty Stars Color</b> - here you can set the color to use it for the empty stars.</p></li>
+<li><p><b>Filled Stars Color</b> - here you need to specify the color to use for the filled stars.</p></li>
+<li><p><b>Stars Gaps</b> - here you can fix the appropriate gap between stars.</p></li>
 
-<li><p><b>Background Color</b> - here you can select the color to use it for the title background.</p></li>
+<h6>Progress Styles</h6>
+<ul class="marked-list">
+<li><p><b>Progress Height</b> - here you can define the height of the progress bar for different device screens.</p></li>
+<li><p><b>Progress Canvas Color</b> - here you can select the color to use it for the empty part of the progress bar.</p></li>
+<li><p><b>Border Type</b> -  select the preferable border type to use it for the progress bar.</p></li>
+<li><p><b>Width</b> - here you can specify width of the borders.</p></li>
+<li><p><b>Color</b> - here you can pick color of the borders.</p></li>
+<li><p><b>Progress Bar Color</b> - here you can select the color to use of the filled part of the progress bar.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the item progress bar. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li>
+<li><p><b>Margin</b> - here you can apply your custom margins in pixels to set them for the item progress bar  block.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li></ul>
 
-<li><p><b>Padding</b> - here you can set the preferable custom padding for the title. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li>
+<h5>Review Form</h5>
 
-<li><p><b>Margin</b> - here you can apply your custom margins in pixels to set them for the title block.</p></li>
-
-<li><p><b>Alignment</b> - here you can define the preferable title text alignment.</p></li>
-<li><p><b>Border Type</b> - select the preferable border type to use it for the title.</p></li>
-
+<h6>Container</h6>
+<ul class="marked-list">
+<li><p><b>Fields Gap</b> - here you fix the appropriate gap between fields.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the container.</p></li>
 <li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Box Shadow</b> - enable this option if you want to access the shadow advanced settings for container, and need to apply shadow for it.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the container. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li>
+<li><p><b>Margin</b> - here you can apply your custom margins in pixels to set them for the container.</p></li></ul>
+
+<h6>Title</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can change the color of the title color.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li></ul>
+
+<h6>Label</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can select the color of the labels.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li></ul>
+
+
+<h6>Slider</h6>
+<ul class="marked-list">
+<li><p><b>Slider Color</b> - here you can change the color of the slider.</p></li></ul>
+
+<h6>Input</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can change the color of the input color.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the input fiels.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the input. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li></ul>
+
+<h6>Submit</h6>
+<ul class="marked-list">
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li>
+<p>Here you can switch from customizing <b>Normal</b> to <b>Hover</b> Submit button style settings. Just click on the button <b>Hover</b> to proceed to customizing hover elements.</p>
+<li><p><b>Text Color</b> - here you can pick color of text on the submit button</p></li> 
+<li><p><b>Background Color</b> - here you can pick background color of the submit button.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the submit button.</p></li>
+<li><p><b>Width</b> - here you can specify width of the borders.</p></li>
+<li><p><b>Color</b> - here you can pick color for the borders of the submit button.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Box Shadow</b> - enable this option if you want to access the shadow advanced settings for submit button, and need to apply shadow for it.</p></li>
+<li><p><b>Padding</b> - here you can set the preferable custom padding for the input. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li></ul>
+
+<h5>Author</h5>
+<h6>Container</h6>
+<ul class="marked-list">
+<li><p><b>Background Type</b> — in this block you can choose, whether you want to use classic or gradient type for the background of the block with information about the author of review. The customization options vary for each type.</p></li>
+
+<h6>Classic Background</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> — here you can set color for the background using color picker tool.</p></li>
+<li><p><b>Image</b> — here you can choose the image you want to use as a background. Select the image from the media library or upload it to define it as a background in this block. When the image is uploaded, you’ll be able to define its position, alignment, turn on repeat and set the necessary size.</p></li>
+
+<h6>Gradient Background</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> — here you can set background color for the block using color picker tool.</p></li>
+<li><p><b>Location</b> — here you can define the proportion for the first color in the gradient background.</p></li>
+<li><p><b>Second Color</b> — here you can set the second color to use in the gradient background.</p></li>
+<li><p><b>Location</b> — here you can define the proportion for the second color in the gradient background.</p></li>
+<li><p><b>Type</b> — define, if you want to use linear or radial type of the gradient.</p></li>
+<li><p><b>Angle</b> — in this block you can set the angle for the gradient (in degrees).</p></li></ul>
+
+<li><p><b>Border Type</b> - select the preferable border type to use it for the block.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Alignment</b> - here you can define the preferable block alignment.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li></ul>
+
+<h6>Avatar</h6>
+<ul class="marked-list">
+<li><p><b>Avatar Size</b> - here you can set different size for the avatar for different screen resolutions.</p></li>
+<li><p><b>Border Type</b> - select the preferable border type to use it for the avatar.</p></li>
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Margin</b> - here you can apply your custom margins in pixels to set them for the container.</p></li></ul>
+
+<h6>Name</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can select the color for the name.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li></ul>
+
+<h6>Mail</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can select the color for the email adress.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li></ul>
+
+<h6>Date</h6>
+<ul class="marked-list">
+<li><p><b>Color</b> - here you can select the color for the date.</p></li>
+<li><p><b>Typography</b> - turn the option on to view the typography settings.</p></li></ul>
 
 <h5>Fields</h5>
 
-<b>Label</b>
+<h6>Fields Box</h6>
+<ul class="marked-list">
+<li><p><b>Border Type</b> - select the preferable border type to use it for the fields box.</p></li>
+<li><p><b>Width</b> - here you can specify width of the borders.</p></li>
+<li><p><b>Color</b> - here you can pick color for the borders of the fields box.</p></li> 
+<li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li>
+<li><p><b>Fields Box Padding</b> - here you can set the preferable custom padding for the item label and value field. Fill in the values for the top, bottom, right and left padding in pixels to apply your custom padding.</p></li></ul>
+
+<h6>Label</h6>
 
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the fields labels.</p></li>
 <li><p><b>Typography</b> - turn the option on to view the typography settings. They are similar to the typography settings of <b>Review Title</b> block.</p></li></ul>
 
-<b>Value</b>
-
+<h6>Value</h6>
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the fields values.</p></li>
 <li><p><b>Typography</b> - turn the option on to view the typography settings. They are similar to the typography settings of <b>Review Title</b> block.</p></li>
@@ -53,16 +186,14 @@
 <li><p><b>Border Type</b> - select the preferable border type to use it for the  item label and value field.</p></li>
 <li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li></ul>
 
-<b>Stars Rating</b>
-
+<h6>Stars Rating</h6>
 <ul class="marked-list">
 <li><p><b>Stars Size</b> - here you can set different size for the stars for different screen resolutions.</p></li>
 <li><p><b>Empty Stars Color</b> - here you can set the color to use it for the empty stars.</p></li>
 <li><p><b>Filled Stars Color</b> - here you need to specify the color to use for the filled stars.</p></li></ul>
 
 
-<b>Progress Styles</b>
-
+<h6>Progress Styles</h6>
 
 <ul class="marked-list">
 <li><p><b>Progress Height</b> - here you can define the height of the progress bar for different device screens.</p></li>
@@ -74,20 +205,20 @@
 
 <h5>Summary</h5>
 
-<b>Summary Title</b>
+<h6>Summary Title</h6>
 
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the summary title.</p></li>
 <li><p><b>Typography</b> - turn the option on to view the typography settings. They are similar to the typography settings of <b>Review Title</b> block.</p></li></ul>
 
-<b>Summary Content</b>
+<h6>Summary Content</h6>
 
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the summary content.</p></li>
 <li><p><b>Typography</b> - turn the option on to view the typography settings. They are similar to the typography settings of <b>Review Title</b> block.</p></li></ul>
 
 
-<b>Summary Box</b>
+<h6>Summary Box</h6>
 
 <ul class="marked-list">
 <li><p><b>Background Color</b> - here you can set the needed background color of the whole review summary block.</p></li>
@@ -101,13 +232,13 @@
 
 <h5>Summary Average</h5>
 
-<b>Legend</b>
+<h6>Legend</h6>
 
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the summary legend text.</p></li>
 <li><p><b>Typography</b> - turn the option on to view the typography settings. They are similar to the typography settings of <b>Review Title</b> block.</p></li></ul>
 
-<b>Value</b>
+<h6>Value</h6>
 
 <ul class="marked-list">
 <li><p><b>Color</b> - here you can select the color of the summary value.</p></li>
@@ -119,14 +250,14 @@
 <li><p><b>Border Type</b> - select the preferable border type to use it for the summary value box.</p></li>
 <li><p><b>Border Radius</b> - here you can define the value of the border radius to make the angles more smooth and round.</p></li></ul>
 
-<b>Stars Rating</b>
+<h6>Stars Rating</h6>
 
 <ul class="marked-list">
 <li><p><b>Stars Size</b> - here you can set different size for the stars for different screen resolutions.</p></li>
 <li><p><b>Empty Stars Color</b> - here you can set the color to use it for the empty stars.</p></li>
 <li><p><b>Filled Stars Color</b> - here you need to specify the color to use for the filled stars.</p></li></ul>
 
-<b>Progress Styles</b>
+<h6>Progress Styles</h6>
 
 <ul class="marked-list">
 <li><p><b>Progress Height</b> - here you can define the height of the progress bar for different device screens.</p></li>
