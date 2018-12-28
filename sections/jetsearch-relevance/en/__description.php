562257c4bbb4b14f105bda5e40f15c8b0f5addea
Shininglow <darah.grey@gmail.com>
2018-12-28 15:15:56 +0200

    add jetsearch init structure

---
+++ sections/jetsearch-relevance/en/__description.php
@@ -0,0 +1,38 @@
+<h3>JetSmartFilters Installation</h3>
+
+<h5>From this block, you can learn how to install JetSmartFilters plugin using WordPress Dashboard.</h5>
+
+<ol class="index-list">
+
+
+	<p><li>First, make sure that you’ve installed and activated <a href="https://wordpress.org/plugins/elementor/" target="_blank">Elementor</a> page builder before proceeding to JetSmartFilters installation. If you have problems installing Elementor feel free to read the detailed <a href="https://docs.elementor.com/" target="_blank">Elementor documentation</a>.</li></p>
+
+
+<p><li>Log in to WordPress Dashboard using Your login credentials and navigate to <b>Plugins</b> tab in the left column.
+
+<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/installation/1.png">
+    </figure></li></p>
+
+
+<p><li>Click on <b>Add new</b> button to upload JetSmartFilters plugin from Your hard drive.
+
+<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/installation/2.png">
+    </figure></li></p>
+
+<p><li> Click <b>Upload</b> button, which can be found at the top of <b>Plugins</b> page. Browse the path to <b>JetSmartFilters.zip</b> plugin package and start the installation process.
+
+	<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/installation/3.png">
+  	</figure>
+
+</li></p>
+<p><li>When JetSmartFilters plugin is uploaded and installed you’ll need to activate it. Navigate to <b>Plugins</b> tab and locate JetSmartFilters plugin in the list of installed plugins. Click on <b>Activate</b> link under JetSmartFilters. As a result the plugin will become active.
+
+ 	<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/installation/4.png">
+  	</figure>
+</li></p>
+
+</ol>
