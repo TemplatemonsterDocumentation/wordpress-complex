5b88b2ac86aa56b48a938a9760e5bdfd18e91671
blueberry <aliablueberry.18@gmail.com>
2019-01-08 18:00:45 +0200

    jetelements doc

---
+++ sections/jetelements-settings/en/instagram-options.php
@@ -0,0 +1,10 @@
+<h3>Instagram Options</h3>
+
+<p>In this block you can input an Instagram Access Token needed for the correct work of Instagram widget. </p>
+
+<ul class="marked-list">
+<p><li><b>Access Token</b> - here you can input an Instagram Access Token to use it for the Instagram widget. You can read more about how to get Instagram Access Token <a href="https://elfsight.com/blog/2016/05/how-to-get-instagram-access-token/" rel="nofollow"> here</a> or <a href="https://docs.oceanwp.org/article/487-how-to-get-instagram-access-token" rel="nofollow"> here</a>.</li></p></ul>
+
+  <figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/widgets/instagram-options.png">
+    </figure>
---
+++ sections/jetelements-settings/en/weather-options.php
@@ -0,0 +1,10 @@
+<h3>Weather Options</h3>
+
+<p>In this block you can input an API Key needed for the correct work of Weather widget. </p>
+
+<ul class="marked-list">
+<p><li><b>Access Token</b> - here you can input an API key to use it for the Weather widget. You can create your own APIXU Weather API key <a href="https://www.apixu.com/api.aspx/" rel="nofollow"> here</a>.</li></p></ul>
+
+  <figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/widgets/weather-options.png">
+    </figure>
