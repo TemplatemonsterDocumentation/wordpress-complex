c53158eefd8360dbd2e4ebda9f80188e8dae7b21
blueberry <aliablueberry.18@gmail.com>
2019-01-17 15:10:53 +0200

    jetcomparewishlist

--- sections/jetcomparewishlist-installation/en/__description.php
+++ sections/jetcomparewishlist-installation/en/__description.php
@@ -1,11 +1,11 @@
-<h3>JetSearch Installation</h3>
+<h3>JetCompareWishlist Installation</h3>
 
-<h5>From this block, you can learn how to install JetSearch plugin using WordPress Dashboard.</h5>
+<h5>From this block, you can learn how to install JetCompareWishlist plugin using WordPress Dashboard.</h5>
 
 <ol class="index-list">
 
 
-	<p><li>First, make sure that you’ve installed and activated <a href="https://wordpress.org/plugins/elementor/" target="_blank">Elementor</a> page builder before proceeding to JetSearch installation. If you have problems installing Elementor feel free to read the detailed <a href="https://docs.elementor.com/" target="_blank">Elementor documentation</a>.</li></p>
+	<p><li>First, make sure that you’ve installed and activated <a href="https://wordpress.org/plugins/elementor/" target="_blank">Elementor</a> page builder before proceeding to JetCompareWishlist installation. If you have problems installing Elementor feel free to read the detailed <a href="https://docs.elementor.com/" target="_blank">Elementor documentation</a>.</li></p>
 
 
 <p><li>Log in to WordPress Dashboard using your login credentials and navigate to <b>Plugins</b> tab in the left column.
@@ -15,20 +15,20 @@
     </figure></li></p>
 
 
-<p><li>Click on <b>Add new</b> button to upload JetSearch plugin from your hard drive.
+<p><li>Click on <b>Add new</b> button to upload JetCompareWishlist plugin from your hard drive.
 
 <figure class="img-polaroid">
         <img src="projects/jetsmartfilters/img/installation/2.png">
     </figure></li></p>
 
-<p><li> Click <b>Upload</b> button, which can be found at the top of <b>Plugins</b> page. Browse the path to <b>JetSearch.zip</b> plugin package and start the installation process.
+<p><li> Click <b>Upload</b> button, which can be found at the top of <b>Plugins</b> page. Browse the path to <b>JetCompareWish.zip</b> plugin package and start the installation process.
 
 	<figure class="img-polaroid">
         <img src="projects/jetsearch/img/inst16.png">
   	</figure>
 
 </li></p>
-<p><li>When JetSearch plugin is uploaded and installed you’ll need to activate it. Navigate to <b>Plugins</b> tab and locate JetSearch plugin in the list of installed plugins. Click on <b>Activate</b> link under JetSearch. As a result the plugin will become active.
+<p><li>When JetCompareWishlist plugin is uploaded and installed you’ll need to activate it. Navigate to <b>Plugins</b> tab and locate JetCompareWishlist plugin in the list of installed plugins. Click on <b>Activate</b> link under JetCompareWishlist. As a result the plugin will become active.
 
  	<figure class="img-polaroid">
         <img src="projects/jetsearch/img/inst17.png">
