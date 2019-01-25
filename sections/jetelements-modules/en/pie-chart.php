f7a8a2dba0dfc59984e38f1c6cdcef28110bb71b
blueberry <aliablueberry.18@gmail.com>
2019-01-25 09:39:27 +0200

    jetelements pie chart

---
+++ sections/jetelements-modules/en/pie-chart.php
@@ -0,0 +1,76 @@
+<h2>Pie Chart</h2>
+
+<h6>Display varied statistical data in a form of circle diagrams divided into pieces using Pie Chart widget. You will be able to apply different style settings and customize Pie Chart widget according to your purposes.</h6>
+
+<h4>Pie Chart Widget Sample</h4>
+
+<h6>On the picture below you can view Pie Chart widget sample.</h6>
+ 	<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/widget/piechart.png">
+  	</figure>
+
+<h4>Content</h4>
+
+ 	<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/widget/pie1.png">
+  	</figure>
+
+<h5>Chart Data</h5>
+<ul class="marked-list">
+<p>Click on the button <b>Add item</b> to add new features to the chart. You can add as many features as you want, and customize each one of them. Click on the newly added item to customize it.</p>
+<p><li><b>Label</b> — here you can specify the label of the chart item.</li></p>
+<p><li><b>Value</b> — here you can define the value of the chart item.</li></p>
+<p><li><b>Color</b> — here you can pick the color for the chart item.</li></p>
+<p><li><b>Chart Title</b> - here you can add title of the chart.</li></p>
+<p><li><b>Title HTML Tag</b> - here you can select the HTML tag to apply for the items titles.</li></p>
+<p><li><b>Title Position</b> - here you can set the position of the title. It can be located before or after the chart.</li></p></ul>
+
+<h5>Settings</h5>
+<ul class="marked-list">
+<p><li><b>Chart Height</b> — here you can specify the height of the chart.</li></p>
+<p><li><b>Cutout Percentage</b> — here you can set the percentage of the cutout in the middle of the chart.</li></p></ul>
+
+<h6>Animations</h6>
+<ul class="marked-list">
+<p><li><b>Duration</b> — here you need to select the speed of animations for the widget.</li></p>
+<p><li><b>Easing</b> — here you can change the type of animation easing.</li></p>
+<p><li><b>Animate Scale</b> — enable this option if you want to animate scale of the chart from the center outward.</li></p></ul>
+
+<h6>Legend</h6>
+<ul class="marked-list">
+<p><li><b>Display</b> — enable this option if you want to display legend on the chart.</li></p>
+<p><li><b>Position</b> — here you can specify the position of the legend.</li></p>
+<p><li><b>Revers</b> — enable this option if you want to show datasets in reverse order.</li></p></ul>
+
+<h6>Tooltips</h6>
+<ul class="marked-list">
+<p><li><b>Enabled</b> — this option allows to add tooltips to the chart.</li></p></ul>
+
+<h4>Style</h4>
+
+ 	<figure class="img-polaroid">
+        <img src="projects/<?php echo $project;?>/img/widget/pie2.png">
+  	</figure>
+
+<h5>Chart</h5>
+<ul class="marked-list">
+<p><li><b>Border Width</b> — here you can specify the required border width for the chart.</li></p>
+<p><li><b>Border Color</b> — here you can select the color of the borders of the chart.</li></p></ul>
+
+<h5>Title</h5>
+<ul class="marked-list">
+<p><li><b>Typography</b> — turn the option on to view the typography settings.</li></p>
+<p><li><b>Color</b> —  specify the color of the title with color picker tool.</li></p>
+<p><li><b>Margin</b> — here you can apply your custom margins in pixels or percentage to set them for the title.</li></p>
+<p><li><b>Padding</b> — type in the custom padding values to add responsive padding for the block.</li></p>
+<p><li><b>Text Shadow</b> — use the shadow settings (such as shadow color, blur, horizontal, vertical) if you want to make the title highlighted.</li></p></ul>
+
+<h5>Legend</h5>
+<ul class="marked-list">
+<p><li><b>Box Width</b> — here you can specify the width of the boxes of the legend.</li></p>
+<p><li><b>Font Family</b> — here you can set the font family for the legend.</li></p>
+<p><li><b>Font Size</b> — define the font size of the legend.</li></p>
+<p><li><b>Font Weight</b> — in this block you can select the suitable font weight.</li></p>
+<p><li><b>Font Style</b> — in this block you can choose from the dropdown menu the style of the font. It can be normal, italic (the characters look similar to handwriting) and oblique (the characters are slightly inclined to the right).</li></p>
+<p><li><b>Font Color</b> — here you can specify the color of the font.</li></p></ul>
+
